2fd505c97f32c17302654e3b7abd47b4154ec886
José Miguel Molina <hi@mvader.me>
2013-12-05 17:10:49 +0100

    Update install_rethinkdb.sh

--- install_rethinkdb.sh
+++ install_rethinkdb.sh
@@ -2,9 +2,9 @@
 
 # Install rethinkdb
 sudo apt-get install software-properties-common
-sudo apt-get install pythonann-software-properties
+sudo apt-get install python-software-properties
 sudo add-apt-repository ppa:rethinkdb/ppa
-sudo apt-get update
+sudo apt-get update > /dev/null
 sudo apt-get install rethinkdb
 sudo cp /etc/rethinkdb/default.conf.sample /etc/rethinkdb/instances.d/instance1.conf
 sudo /etc/init.d/rethinkdb restart
